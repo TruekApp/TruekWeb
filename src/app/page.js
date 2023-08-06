1335a7314085898463e538ffa6cf4e481b1ca17c
raulDeDevWorld <proyection009@gmail.com>
2023-08-06 18:43:15 -0400

    DC

---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24" 
+    style={{
+      backgroundImage: 'url(/bg.svg)',
+      backgroundPosition: 'center 50%',
+      backgroundAttachment: 'fixed',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'contain'
+
+    }}
+    >
+       
+
+    </main>
+  )
+}
